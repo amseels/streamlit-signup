3b4a2695747785354599839e37dd43add064f420
Nuril Kaunaini R <aini2403@gmail.com>
2023-12-24 20:42:08 +0700

    .

--- streamlit_login_auth_ui/widgets.py
+++ streamlit_login_auth_ui/widgets.py
@@ -239,7 +239,7 @@ class __login__:
                 menu_icon = 'list-columns-reverse',
                 icons = ['box-arrow-in-right', 'person-plus', 'x-circle','arrow-counterclockwise'],
                 # options = ['Login', 'Create Account', 'Forgot Password?', 'Reset Password'],
-                options = ['Login', 'Create Account', 'Forgot Password?'],
+                options = ['Login', 'Create Account'],
                 styles = {
                     "container": {"padding": "5px"},
                     "nav-link": {"font-size": "14px", "text-align": "left", "margin":"0px"}} )
