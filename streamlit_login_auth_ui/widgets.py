b42d1e650e3e4e6e106aada4703293c5c6d7cfcf
Nuril Kaunaini R <aini2403@gmail.com>
2023-12-25 20:20:00 +0700

    remove name and email at signup page

--- streamlit_login_auth_ui/widgets.py
+++ streamlit_login_auth_ui/widgets.py
@@ -106,12 +106,6 @@ class __login__:
         Creates the sign-up widget and stores the user info in a secure way in the _secret_auth_.json file.
         """
         with st.form("Sign Up Form"):
-            name_sign_up = st.text_input("Name *", placeholder = 'Please enter your name')
-            valid_name_check = check_valid_name(name_sign_up)
-
-            email_sign_up = st.text_input("Email *", placeholder = 'Please enter your email')
-            valid_email_check = check_valid_email(email_sign_up)
-            unique_email_check = check_unique_email(email_sign_up)
             
             username_sign_up = st.text_input("Username *", placeholder = 'Enter a unique username')
             unique_username_check = check_unique_usr(username_sign_up)
@@ -122,16 +116,7 @@ class __login__:
             sign_up_submit_button = st.form_submit_button(label = 'Register')
 
             if sign_up_submit_button:
-                if valid_name_check == False:
-                    st.error("Please enter a valid name!")
-
-                elif valid_email_check == False:
-                    st.error("Please enter a valid Email!")
-                
-                elif unique_email_check == False:
-                    st.error("Email already exists!")
-                
-                elif unique_username_check == False:
+                if unique_username_check == False:
                     st.error(f'Sorry, username {username_sign_up} already exists!')
                 
                 elif unique_username_check == None:
@@ -143,15 +128,11 @@ class __login__:
                         color = "green" if passed else "red"
                         symbol = "✔️" if passed else "❌"
                         st.markdown(f"<span style='color: {color};'>{symbol} {criterion.capitalize()}</span>", unsafe_allow_html=True)
-
-
-                if valid_name_check == True:
-                    if valid_email_check == True:
-                        if unique_email_check == True:
-                            if unique_username_check == True:
-                                if password_strength == "STRONG":
-                                    register_new_usr(name_sign_up, email_sign_up, username_sign_up, password_sign_up)
-                                    st.success("Registration Successful!")
+    
+                if unique_username_check == True:
+                    if password_strength == "STRONG":
+                        register_new_usr(username_sign_up, password_sign_up)
+                        st.success("Registration Successful!")
                 
 
     def forgot_password(self) -> None:
